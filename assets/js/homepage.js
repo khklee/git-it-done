409332ed1f27e8fb53381ac058164e61aebf2779
Kelly Lee <amorfati38@gmail.com>
2022-01-15 21:07:22 -0700

    add git repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,16 +8,6 @@ var getUserRepos = function (user) {
             console.log(data);
         })
     })
-    // var response = fetch("https://api.github.com/users/octocat/repos");
-    // console.log(response);
-
-    // fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-    //     response.json().then(function(data) {
-    //         console.log(data);
-    //     })
-    //     console.log("inside", response);
-    // })
-    // console.log("outside")
 }
 
 getUserRepos();
